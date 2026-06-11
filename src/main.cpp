2c60e5e6f7dfff52a7f7b3f4174cfe8a7501fcb2
dtilm <d.tilman05@gmail.com>
2026-06-11 14:38:51 +0100

    fix uniform grid child height calculation

--- src/main.cpp
+++ src/main.cpp
@@ -16,18 +16,35 @@ protected:
         grid->columns = 2;
         grid->rows = 2;
 
+        Rectangle *red2 = new Rectangle();
+        Rectangle *green2 = new Rectangle();
+        Rectangle *blue2 = new Rectangle();
+        Rectangle *yellow2 = new Rectangle();
+
         Rectangle *red = new Rectangle();
         Rectangle *green = new Rectangle();
         Rectangle *blue = new Rectangle();
-        Rectangle *yellow = new Rectangle();
 
         red->fill = Colors::Red;
         green->fill = Colors::Green;
         blue->fill = Colors::Blue;
-        yellow->fill = Colors::Yellow;
+
+        red2->fill = Colors::Red;
+        green2->fill = Colors::Green;
+        blue2->fill = Colors::Blue;
+        yellow2->fill = Colors::Yellow;
+
+        UniformGrid *grid2 = new UniformGrid();
+        grid2->columns = 4;
+        grid2->rows = 1;
+
+        grid2->AddChild(red2, 0, 0);
+        grid2->AddChild(yellow2, 1, 0);
+        grid2->AddChild(green2, 2, 0);
+        grid2->AddChild(blue2, 3, 0);
 
         grid->AddChild(red, 0, 0);
-        grid->AddChild(yellow, 1, 0);
+        grid->AddChild(grid2, 1, 0);
         grid->AddChild(green, 0, 1);
         grid->AddChild(blue, 1, 1);
 
